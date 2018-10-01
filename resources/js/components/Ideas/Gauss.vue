f9256e23a93e730e3dd6df67ca697841cce80507
Matthias Schwebler <m.schwebler@laola1.at>
2018-10-01 16:34:31 +0200

    Make gauss customizable.

--- resources/js/components/Ideas/Gauss.vue
+++ resources/js/components/Ideas/Gauss.vue
@@ -20,12 +20,38 @@
                     datasets: [
                         {
                             label: 'Energy',
-                            // backgroundColor: 'rgba(248, 121, 121, .1)',
+                            backgroundColor: 'rgba(121, 121, 121, .1)',
                             borderColor: '#17a089',
-                            data: this.data
+                            data: this.data,
+                            pointBorderColor: "rgba(0,0,0,0)",
+                            pointBackgroundColor: "rgba(0,0,0,0)",
+                            pointRadius: 0,
+                            pointHitRadius: 0,
+                            pointHoverRadius: 0
                         }
                     ]
-                }, {responsive: true, maintainAspectRatio: false});
+                },
+                {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    animation: false,
+                    scales: {
+                        yAxes: [{
+                            gridLines: {
+                                drawBorder: false,
+                            },
+                            ticks: {
+                                beginAtZero: true,
+                                display: false
+                            }
+                        }],
+                        xAxes: [{
+                            gridLines: {
+                                display: false,
+                            },
+                        }],
+                    },
+                });
             }
         }
     }
--- resources/js/components/Ideas/GaussWrapper.vue
+++ resources/js/components/Ideas/GaussWrapper.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <gauss :data="data" :labels="labels" ref="chart"></gauss>
+        Erwartungswert: {{ mean }}
+        <input type="range" min="0" max="100" :step="stepSize" style="width: 100%" v-model="mean"/>
+        Standartabweichung: {{ stdDev }}
+        <input type="range" min="1" max="100" step="1" style="width: 100%" v-model="stdDev"/>
     </div>
 </template>
 
@@ -11,7 +15,8 @@
             return {
                 chartData: [],
                 stdDev: 20,  // Standard deviation => Standardabweichung
-                mean: 80  // => Erwartungswert
+                mean: 100,  // => Erwartungswert
+                stepSize: 5
             }
         },
         methods: {
@@ -36,8 +41,16 @@
             labels: {
                 get() {
                     let labels = [];
-                    // this.chartData.forEach(() => labels.push(Math.round((x++ * 100 / this.chartData.length) * 10) / 10));
-                    this.chartData.forEach(() => labels.push(''));
+                    let x = 0;
+                    this.chartData.forEach(() => {
+                        // step += this.stepSize;
+                        x++;
+                        if (x % this.stepSize === 0) {
+                            labels.push(x);
+                        } else {
+                            labels.push('')
+                        }
+                    });
                     return labels;
                 },
                 set() {

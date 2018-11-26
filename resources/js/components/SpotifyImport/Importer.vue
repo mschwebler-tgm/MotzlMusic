29de458f17444ab3d73679c5d2c24bb615f4154f
Matthias Schwebler <m.schwebler@laola1.at>
2018-11-26 11:08:30 +0100

    Adds frontend spotify importer.

--- resources/js/components/SpotifyImport/Importer.vue
+++ resources/js/components/SpotifyImport/Importer.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="modal-card">
+    <div class="modal-card spotify">
         <header class="modal-card-head">
             <img src="/images/spotify_black.png" width="50px">
-            <p class="modal-card-title">Import from Spotify</p>
+            <p class="modal-card-title">&nbsp;&nbsp;Import from Spotify</p>
         </header>
         <section class="modal-card-body">
-            CONTEEEENT
+            You can import your favourite tracks, albums and playlists from spotify.<br><br>
+            <div class="field">
+                <b-checkbox>Import saved tracks</b-checkbox>
+            </div>
+            <div class="field">
+                <b-checkbox>Import albums</b-checkbox>
+            </div>
+            <div class="field">
+                <b-checkbox>Import playlists</b-checkbox>
+            </div>
         </section>
         <footer class="modal-card-foot">
-            <button class="button is-success">Save changes</button>
+            <button class="button is-success">Import</button>
             <button class="button">Cancel</button>
         </footer>
     </div>

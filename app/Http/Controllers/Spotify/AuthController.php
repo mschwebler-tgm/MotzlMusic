2d79dadfc1054f3bafaf2e8d8d3296b341bf8f02
Matthias Schwebler <m.schwebler@laola1.at>
2018-11-28 12:51:42 +0100

    Retry oauth login if invalid state is returned.

--- app/Http/Controllers/Spotify/AuthController.php
+++ app/Http/Controllers/Spotify/AuthController.php
@@ -6,6 +6,7 @@ use App\Daos\UserDao;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\InvalidStateException;
 
 class AuthController extends Controller
 {
@@ -16,8 +17,12 @@ class AuthController extends Controller
 
     public function authorizeUser(UserDao $userDao)
     {
-        /** @var \SocialiteProviders\Manager\OAuth2\User $oauth2User */
-        $oauth2User = Socialite::driver('spotify')->user();
+        try {
+            /** @var \SocialiteProviders\Manager\OAuth2\User $oauth2User */
+            $oauth2User = Socialite::driver('spotify')->user();
+        } catch (InvalidStateException $e) {
+            return redirect('/spotify/authorize');
+        }
         $user = $userDao->createFromSpotifyAuthorization($oauth2User);
         Auth::login($user, true);
         return redirect('/');

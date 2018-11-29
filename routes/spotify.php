acf1589675ab7ecc050d83538824d9764312cebd
Matthias Schwebler <m.schwebler@laola1.at>
2018-11-29 11:35:16 +0100

    Removes unused urls.

--- routes/spotify.php
+++ routes/spotify.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/authorize', 'AuthController@requestAuthCode');
 Route::get('/authorize/landing', 'AuthController@authorizeUser');
-
-# Import
-Route::get('/tracks/my', 'ImportController@tracks');
-Route::get('/playlists/my', 'ImportController@playlists');

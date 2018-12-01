ada2d5e5176374afe868a190aa1831238848cfd1
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2018-12-01 14:31:51 +0100

    make attributes public.

--- app/Components/Spotify/Models/Track.php
+++ app/Components/Spotify/Models/Track.php
@@ -9,39 +9,39 @@ class Track extends BaseModel
     protected $apiResponseTrack;
 
     /** @var string */
-    protected $id;
+    public $id;
     /** @var string */
-    protected $name;
+    public $name;
     /** @var int */
-    protected $duration;
+    public $duration;
     /** @var boolean */
-    protected $explicit;
+    public $explicit;
     /** @var string */
-    protected $href;
+    public $href;
     /** @var boolean */
-    protected $isLocal;
+    public $isLocal;
     /** @var boolean */
-    protected $isPlayable;
+    public $isPlayable;
     /** @var integer */
-    protected $popularity;
+    public $popularity;
     /** @var string */
-    protected $uri;
+    public $uri;
     /** @var Album */
-    protected $album;
+    public $album;
     /** @var Collection */
-    protected $artists;
+    public $artists;
     /** @var int */
-    protected $discNumber;
+    public $discNumber;
     /** @var object */
-    protected $externalIds;
+    public $externalIds;
     /** @var object */
-    protected $externalUrls;
+    public $externalUrls;
     /** @var string */
-    protected $previewUrl;
+    public $previewUrl;
     /** @var int */
-    protected $trackNumber;
+    public $trackNumber;
     /** @var string */
-    protected $type;
+    public $type;
 
     public function __construct($apiResponseTrack)
     {
@@ -83,12 +83,8 @@ class Track extends BaseModel
         $this->artists = $artistCollection;
     }
 
-    # ----------------------------
-    #      getters and setters
-    # ----------------------------
-
     /**
-     * @return mixed
+     * @return object
      */
     public function getApiResponseTrack()
     {
@@ -96,282 +92,11 @@ class Track extends BaseModel
     }
 
     /**
-     * @param mixed $apiResponseTrack
+     * @param object $apiResponseTrack
      */
     public function setApiResponseTrack($apiResponseTrack): void
     {
         $this->apiResponseTrack = $apiResponseTrack;
     }
 
-    /**
-     * @return string
-     */
-    public function getId(): string
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param string $id
-     */
-    public function setId(string $id): void
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param string $name
-     */
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return int
-     */
-    public function getDuration(): int
-    {
-        return $this->duration;
-    }
-
-    /**
-     * @param int $duration
-     */
-    public function setDuration(int $duration): void
-    {
-        $this->duration = $duration;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isExplicit(): bool
-    {
-        return $this->explicit;
-    }
-
-    /**
-     * @param bool $explicit
-     */
-    public function setExplicit(bool $explicit): void
-    {
-        $this->explicit = $explicit;
-    }
-
-    /**
-     * @return string
-     */
-    public function getHref(): string
-    {
-        return $this->href;
-    }
-
-    /**
-     * @param string $href
-     */
-    public function setHref(string $href): void
-    {
-        $this->href = $href;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isLocal(): bool
-    {
-        return $this->isLocal;
-    }
-
-    /**
-     * @param bool $isLocal
-     */
-    public function setIsLocal(bool $isLocal): void
-    {
-        $this->isLocal = $isLocal;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isPlayable(): bool
-    {
-        return $this->isPlayable;
-    }
-
-    /**
-     * @param bool $isPlayable
-     */
-    public function setIsPlayable(bool $isPlayable): void
-    {
-        $this->isPlayable = $isPlayable;
-    }
-
-    /**
-     * @return int
-     */
-    public function getPopularity(): int
-    {
-        return $this->popularity;
-    }
-
-    /**
-     * @param int $popularity
-     */
-    public function setPopularity(int $popularity): void
-    {
-        $this->popularity = $popularity;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUri(): string
-    {
-        return $this->uri;
-    }
-
-    /**
-     * @param string $uri
-     */
-    public function setUri(string $uri): void
-    {
-        $this->uri = $uri;
-    }
-
-    /**
-     * @return Album
-     */
-    public function getAlbum(): Album
-    {
-        return $this->album;
-    }
-
-    /**
-     * @param Album $album
-     */
-    public function setAlbum(Album $album): void
-    {
-        $this->album = $album;
-    }
-
-    /**
-     * @return Collection
-     */
-    public function getArtists(): Collection
-    {
-        return $this->artists;
-    }
-
-    /**
-     * @param Collection $artists
-     */
-    public function setArtists(Collection $artists): void
-    {
-        $this->artists = $artists;
-    }
-
-    /**
-     * @return int
-     */
-    public function getDiscNumber(): int
-    {
-        return $this->discNumber;
-    }
-
-    /**
-     * @param int $discNumber
-     */
-    public function setDiscNumber(int $discNumber): void
-    {
-        $this->discNumber = $discNumber;
-    }
-
-    /**
-     * @return object
-     */
-    public function getExternalIds(): object
-    {
-        return $this->externalIds;
-    }
-
-    /**
-     * @param object $externalIds
-     */
-    public function setExternalIds(object $externalIds): void
-    {
-        $this->externalIds = $externalIds;
-    }
-
-    /**
-     * @return object
-     */
-    public function getExternalUrls(): object
-    {
-        return $this->externalUrls;
-    }
-
-    /**
-     * @param object $externalUrls
-     */
-    public function setExternalUrls(object $externalUrls): void
-    {
-        $this->externalUrls = $externalUrls;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPreviewUrl(): string
-    {
-        return $this->previewUrl;
-    }
-
-    /**
-     * @param string $previewUrl
-     */
-    public function setPreviewUrl(string $previewUrl): void
-    {
-        $this->previewUrl = $previewUrl;
-    }
-
-    /**
-     * @return int
-     */
-    public function getTrackNumber(): int
-    {
-        return $this->trackNumber;
-    }
-
-    /**
-     * @param int $trackNumber
-     */
-    public function setTrackNumber(int $trackNumber): void
-    {
-        $this->trackNumber = $trackNumber;
-    }
-
-    /**
-     * @return string
-     */
-    public function getType(): string
-    {
-        return $this->type;
-    }
-
-    /**
-     * @param string $type
-     */
-    public function setType(string $type): void
-    {
-        $this->type = $type;
-    }
 }

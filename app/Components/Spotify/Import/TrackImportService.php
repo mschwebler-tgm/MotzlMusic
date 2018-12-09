c109a6c284076017d5090bc64b29aae5974d7534
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2018-12-09 13:50:46 +0100

    Dispatches refinement jobs for tracks, albums and artists during import.

--- app/Components/Spotify/Import/TrackImportService.php
+++ app/Components/Spotify/Import/TrackImportService.php
@@ -2,12 +2,22 @@
 
 namespace App\Components\Spotify\Import;
 
+use App\Album;
 use App\Components\Spotify\Models\Track as SpotifyTrack;
+use App\Components\Spotify\Refinement\Jobs\RefineAlbumsJob;
+use App\Components\Spotify\Refinement\Jobs\RefineArtistsJob;
+use App\Components\Spotify\Refinement\Jobs\RefineTracksJob;
+use App\Track;
 use Illuminate\Support\Collection;
 
 class TrackImportService
 {
     private $trackImporterDao;
+    private $trackRefinementQueue = [];
+    private $albumRefinementQueue = [];
+    private $artistRefinementQueue = [];
+
+    const MAX_QUEUE_LIMIT = 20;
 
     public function __construct(TrackImporterDao $trackImporterDao)
     {
@@ -21,6 +31,47 @@ class TrackImportService
             $track = $this->trackImporterDao->storeTrack($spotifyTrack, apiUser());
             $album = $this->trackImporterDao->storeAlbum($spotifyTrack->album);
             $artists = $this->trackImporterDao->storeArtists($spotifyTrack->artists);
+            $this->addToRefinementQueue($track, $album, $artists);
         }
+        $this->dispatchRefinementJobs(true);
+    }
+
+    private function addToRefinementQueue(Track $track, Album $album, Collection $artists)
+    {
+        $this->trackRefinementQueue = array_merge($this->trackRefinementQueue, [$track->id]);
+        $this->albumRefinementQueue = array_merge($this->albumRefinementQueue, [$album->id]);
+        $this->artistRefinementQueue = array_merge($this->artistRefinementQueue, [$artists->pluck('id')]);
+        $this->dispatchRefinementJobs();
+    }
+
+    private function dispatchRefinementJobs($force = false)
+    {
+        if ($force || count($this->trackRefinementQueue) >= self::MAX_QUEUE_LIMIT) {
+            $this->dispatchTrackRefinementJob();
+        }
+        if ($force || count($this->albumRefinementQueue) >= self::MAX_QUEUE_LIMIT) {
+            $this->dispatchAlbumRefinementJob();
+        }
+        if ($force || count($this->artistRefinementQueue) >= self::MAX_QUEUE_LIMIT) {
+            $this->dispatchArtistRefinementJob();
+        }
+    }
+
+    private function dispatchTrackRefinementJob()
+    {
+        RefineTracksJob::dispatch($this->albumRefinementQueue)->onQueue('prio_low');
+        $this->trackRefinementQueue = [];
+    }
+
+    private function dispatchAlbumRefinementJob()
+    {
+        RefineAlbumsJob::dispatch($this->albumRefinementQueue)->onQueue('prio_low');
+        $this->albumRefinementQueue = [];
+    }
+
+    private function dispatchArtistRefinementJob()
+    {
+        RefineArtistsJob::dispatch($this->albumRefinementQueue)->onQueue('prio_low');
+        $this->artistRefinementQueue = [];
     }
 }
---
+++ app/Components/Spotify/Refinement/Jobs/RefineTracksJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Components\Spotify\Refinement\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class RefineTracksJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $albumIds;
+
+    public function __construct($albumIds)
+    {
+        $this->albumIds = $albumIds;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // TODO implement
+    }
+}

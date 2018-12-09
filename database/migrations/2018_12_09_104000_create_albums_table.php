e3ba85f9bd183d162d960e920422a6bad5a8347d
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2018-12-09 11:56:49 +0100

    creates album, artist and track table.

---
+++ database/migrations/2018_12_09_104000_create_albums_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('popularity')->nullable();
+            $table->string('label')->nullable();
+            $table->date('release_date')->nullable();
+            $table->unsignedInteger('total_tracks')->nullable();
+            $table->string('spotify_id')->nullable();
+            $table->string('spotify_href')->nullable();
+            $table->string('spotify_uri')->nullable();
+            $table->string('spotify_image_small')->nullable();
+            $table->string('spotify_image_medium')->nullable();
+            $table->string('spotify_image_large')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
---
+++ database/migrations/2018_12_09_104028_create_tracks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('popularity')->nullable();
+            $table->unsignedInteger('album_id')->nullable();
+            $table->string('spotify_id')->nullable();
+            $table->string('spotify_href')->nullable();
+            $table->string('spotify_uri')->nullable();
+            $table->timestamps();
+
+            $table->foreign('album_id')->references('id')->on('albums');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+}

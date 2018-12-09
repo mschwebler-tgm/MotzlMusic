70320b2af5b20a309be2add9c43e71f11d824b38
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2018-12-09 12:10:20 +0100

    Adds user id to tracks table.

--- database/migrations/2018_12_09_104028_create_tracks_table.php
+++ database/migrations/2018_12_09_104028_create_tracks_table.php
@@ -18,12 +18,14 @@ class CreateTracksTable extends Migration
             $table->string('name');
             $table->unsignedInteger('popularity')->nullable();
             $table->unsignedInteger('album_id')->nullable();
+            $table->unsignedInteger('user_id');
             $table->string('spotify_id')->nullable();
             $table->string('spotify_href')->nullable();
             $table->string('spotify_uri')->nullable();
             $table->timestamps();
 
             $table->foreign('album_id')->references('id')->on('albums');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 

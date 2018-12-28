d89736eb7dc59d49b68fcb49a5e235bb9c503945
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2018-12-28 20:43:01 +0100

    Clips text and adds title attribute.

--- resources/js/store/modules/myLibrary/getters.js
+++ resources/js/store/modules/myLibrary/getters.js
@@ -8,10 +8,10 @@ export default {
 function clusterizeTracks(tracks) {
     return tracks.reduce((rows, track, index) => rows.concat([
         `<div class="track">` +
-            `<div class="track-list-number">&nbsp;${index + 1}</div>` +
-            `<div class="track-list-title">&nbsp;${track.name}</div>` +
-            `<div class="track-list-duration">&nbsp;${track.duration_formatted}</div>` +
-            `<div class="track-list-artist">&nbsp;${track.artist.name}</div>` +
+            `<div class="track-list-number">&nbsp;<span>${index + 1}</span></div>` +
+            `<div class="track-list-title">&nbsp;<span title="${track.name}">${track.name}</span></div>` +
+            `<div class="track-list-duration">&nbsp;<span>${track.duration_formatted}</span></div>` +
+            `<div class="track-list-artist">&nbsp;<span title="${track.artist.name}">${track.artist.name}</span></div>` +
         `</div>`
         ]), []);
 }

ba9f8e5864d395de4ea6c074723a342417605530
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2018-12-28 23:46:31 +0100

    Adds point.

--- resources/js/util/clusterizeTracks.js
+++ resources/js/util/clusterizeTracks.js
@@ -1,7 +1,7 @@
 const clusterizeTracks = function (tracks) {
     return tracks.reduce((rows, track, index) => rows.concat([
         `<div class="track" data-id="${track.id}">` +
-        `<div class="track-list-number">&nbsp;<span>${index + 1}</span></div>` +
+        `<div class="track-list-number">&nbsp;<span>${index + 1}.</span></div>` +
         `<div class="track-list-title">&nbsp;<span title="${track.name}">${track.name}</span></div>` +
         `<div class="track-list-duration">&nbsp;<span>${track.duration_formatted}</span></div>` +
         `<div class="track-list-artist">&nbsp;<span title="${track.artist.name}">${track.artist.name}</span></div>` +

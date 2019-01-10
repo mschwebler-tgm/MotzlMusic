29c366583c64e6b345d07a5297ef8ef7e372f190
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-01-10 21:30:05 +0100

    Refactor: extract artist card to own vue file.

--- resources/js/components/MyLibrary/Artists.vue
+++ resources/js/components/MyLibrary/Artists.vue
@@ -3,35 +3,18 @@
         <b-loading :active="!topArtistsInitialized" :is-full-page="false"></b-loading>
         <h2 class="has-text-weight-light title is-9">Top Artists</h2>
         <div v-if="topArtistsInitialized" class="artists-wrapper">
-            <div class="card" v-for="artist in topArtists">
-                <div class="card-image">
-                    <figure class="image is-4by3">
-                        <img :src="artist.spotify_image_medium" :alt="artist.name">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-content">
-                            <p class="title is-5">{{ artist.name }}</p>
-                            <p class="subtitle is-6">
-                                <b-icon icon="music" size="is-small"></b-icon>
-                                {{ artist.track_amount }}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <artist-item :artist="artist" v-for="artist in topArtists"></artist-item>
         </div>
     </div>
 </template>
 
 <script>
     import BLoading from "buefy/src/components/loading/Loading";
-    import BIcon from "buefy/src/components/icon/Icon";
+    import ArtistItem from "./ArtistItem";
 
     export default {
         name: "artist",
-        components: {BIcon, BLoading},
+        components: {ArtistItem, BLoading},
         computed: {
             topArtists() {
                 return this.$store.getters['myLibrary/topArtists'];
@@ -48,16 +31,6 @@
     .artists-wrapper {
         display: flex;
         flex-wrap: wrap;
-    }
-
-    .card {
-        width: 200px;
-        margin-right: 20px;
-        margin-bottom: 20px;
-    }
-
-    .image > img {
-        object-fit: cover;
     }
 
 </style>

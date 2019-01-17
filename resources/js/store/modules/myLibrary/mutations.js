0f9dbc8490bb5209442bd2d5564c9068aa2caf76
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-01-17 21:14:51 +0100

    Prevents memory leaks by type checking array.

--- resources/js/store/modules/myLibrary/mutations.js
+++ resources/js/store/modules/myLibrary/mutations.js
@@ -1,22 +1,26 @@
+const validateArray = function (toCheck) {
+    return Array.isArray(toCheck) ? toCheck : [];
+};
+
 export default {
     setMyPlaylists(state, playlists) {
-        state.playlists = playlists;
+        state.playlists = validateArray(playlists);
         state.playlistsInitialized = true;
     },
     setMyTracks(state, tracks) {
-        state.tracks = tracks;
+        state.tracks = validateArray(tracks);
         state.tracksInitialized = true;
     },
     setMyTopArtists(state, artists) {
-        state.topArtists = artists;
+        state.topArtists = validateArray(artists);
         state.topArtistsInitialized = true;
     },
     setMyRecentArtists(state, artists) {
-        state.recentArtists = artists;
+        state.recentArtists = validateArray(artists);
         state.recentArtistsInitialized = true;
     },
     setMyAlbums(state, albums) {
-        state.albums = albums;
+        state.albums = validateArray(albums);
         state.albumsInitialized = true;
     },
 }

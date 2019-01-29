21840425b525939f3cc65cf95f3eb34d0e7039c0
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2019-01-29 21:01:21 +0100

    Upload: Shows checkmark after upload is complete.

--- resources/js/components/Upload/UploadInfoBox.vue
+++ resources/js/components/Upload/UploadInfoBox.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="box" v-if="show">
+    <div class="box" v-if="show || true">
         <h3 class="title is-6">New tracks incoming! 😎
             <!--<b-icon icon="emoticon-cool-outline" type="is-primary"></b-icon>-->
         </h3>
-        {{ doneFilesCount }}/{{ totalFilesCount }} Files
-        <div class="flex-center">
-            <progress class="flex-1 progress is-primary is-small custom-progress"
-                      :value="doneFilesCount"
-                      :max="totalFilesCount"></progress>
-            <b-icon icon="loading" custom-class="fa-spin" type="is-primary"></b-icon>
+        <div class="loader-wrapper">
+            <template v-if="uploadInProgress">
+                {{ doneFilesCount }}/{{ totalFilesCount }} Files
+                <div class="flex-center">
+                    <progress class="flex-1 progress is-primary is-small custom-progress"
+                              :value="doneFilesCount"
+                              :max="totalFilesCount"></progress>
+                    <b-icon icon="loading" custom-class="fa-spin" type="is-primary"></b-icon>
+                </div>
+            </template>
+            <template v-else>
+                <div class="position-relative flex-center w-100">
+                    <div class="checkmark draw" :class="{show: !uploadInProgress}"></div>
+                </div>
+            </template>
         </div>
     </div>
 </template>
@@ -29,13 +38,27 @@
             },
             totalFilesCount() {
                 return this.$store.getters['fileUpload/totalFilesCount'];
+            },
+            uploadInProgress() {
+                return this.$store.getters['fileUpload/uploadInProgress'];
             }
         }
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    @import "../../../sass/components/uploadInfoBox.scss";
+
     .custom-progress {
         margin: 0 10px 0 0;
     }
+
+    .loader-wrapper {
+        height: $loader-size;
+    }
+
+    .show {
+        display: block;
+    }
+
 </style>

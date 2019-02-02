5e69a4a0e7fecd8f3766794265edf4ccb8598c25
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-02-02 14:00:57 +0100

    Upload: Returns spotify match. WIP

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -2,17 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Components\Upload\SingleUploadRequest;
 use App\Components\Upload\SpotifyMatcher;
-use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
-    public function uploadTrack(Request $request, SpotifyMatcher $spotifyMatcher)
+    public function uploadTrack(SingleUploadRequest $request, SpotifyMatcher $spotifyMatcher)
     {
-        $mp3File = $request->file('track');
-        $trackName = pathinfo($mp3File->getClientOriginalName(), PATHINFO_FILENAME);
-        $spotifyMatcher->getEstimatedSpotifyMatch($trackName);
-        dd($mp3File);
+        $trackName = $request->getFileName();
+        $spotifyTrack = $spotifyMatcher->getEstimatedSpotifyMatch($trackName);
+        dd($spotifyTrack);
 //        $a = $mp3File->store(apiUser()->mp3StoragePath());
         return response(null, 200);
     }

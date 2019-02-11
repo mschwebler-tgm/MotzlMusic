9defbe4a31b7ec9c50a33ac43b8d91a60703ff4f
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-02-11 23:08:21 +0100

    Puts my library content into cards.

--- resources/js/components/MyLibrary/MyLibrary.vue
+++ resources/js/components/MyLibrary/MyLibrary.vue
@@ -1,27 +1,12 @@
 <template>
-    <router-view></router-view>
+    <v-card>
+        <router-view></router-view>
+    </v-card>
 </template>
 
 <script>
-    import {mapGetters} from 'vuex';
-
     export default {
         name: "MyLibrary",
-        computed: {
-            ...mapGetters('myLibrary', [
-                'playlists',
-                'playlistsInitialized',
-                'tracks',
-                'tracksInitialized',
-                'tracksClusterized',
-                'topArtists',
-                'topArtistsInitialized',
-                'recentArtists',
-                'recentArtistsInitialized',
-                'albums',
-                'albumsInitialized',
-            ]),
-        }
     }
 </script>
 
--- resources/js/components/MyLibrary/Playlists/Playlists.vue
+++ resources/js/components/MyLibrary/Playlists/Playlists.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
+        <v-card-title class="headline">Playlists</v-card-title>
         <v-progress-circular
-                v-if="!$parent.playlistsInitialized"
+                v-if="!playlistsInitialized"
                 color="primary"
                 indeterminate>
         </v-progress-circular>
-        <span class="headline">Playlists</span>
+
         <v-container grid-list-lg>
             <v-layout row wrap>
-                <v-flex v-for="playlist in $parent.playlists"
+                <v-flex v-for="playlist in playlists"
                         :key="playlist.id"
                         xs12 sm6 md4 lg4 xl3 d-block justify-center>
                     <playlist-item :playlist="playlist"></playlist-item>
@@ -20,10 +21,17 @@
 
 <script>
     import PlaylistItem from "./PlaylistItem";
+    import {mapGetters} from 'vuex';
 
     export default {
         name: "Playlists",
-        components: {PlaylistItem}
+        components: {PlaylistItem},
+        computed: {
+            ...mapGetters('myLibrary', [
+                'playlists',
+                'playlistsInitialized',
+            ]),
+        }
     }
 </script>
 

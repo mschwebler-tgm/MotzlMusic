08b65400295f97ee27d26ada4c51e8c9f9095228
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-02-11 22:40:58 +0100

    Adds title to playlist cards. WIP

--- resources/js/components/MyLibrary/Playlists/PlaylistItem.vue
+++ resources/js/components/MyLibrary/Playlists/PlaylistItem.vue
@@ -3,7 +3,8 @@
         <v-img width="40%"
                :src="fullResolutionImage"
                :lazy-src="intermediateImage"
-               :alt="playlist.name">
+               :alt="playlist.name"
+               style="display: inline-block;">
             <v-layout
                     slot="placeholder"
                     fill-height
@@ -13,6 +14,11 @@
                 <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
             </v-layout>
         </v-img>
+        <div style="display: inline-block; top: -10px; position: absolute;">
+            <v-card-title primary-title>
+                <h3 class="title mb-0">{{ playlist.name }}</h3>
+            </v-card-title>
+        </div>
     </v-card>
 </template>
 

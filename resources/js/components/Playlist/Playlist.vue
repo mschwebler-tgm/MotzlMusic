605fc727f734edfa65ff1eea478351d8468e2d99
Matthias Schwebler <m.schwebler@laola1.at>
2019-03-11 17:42:44 +0100

    Displays playlist image, name and type.

--- resources/js/components/Playlist/Playlist.vue
+++ resources/js/components/Playlist/Playlist.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <!-- ERROR -->
         <div v-if="errorResponse" class="playlist-error">
             <div>
                 <div class="error">
@@ -13,6 +14,31 @@
             </div>
             <img src="/images/403.svg" alt="403" width="400">
         </div>
+
+        <!-- PLAYLIST -->
+        <template v-if="playlist">
+            <div class="d-flex">
+                <v-img
+                        :src="playlist.spotify_image_medium"
+                        :lazy-src="playlist.spotify_image_small"
+                        aspect-ratio="1"
+                        class="grey lighten-2 playlist-image">
+                    <template v-slot="placeholder">
+                        <v-layout
+                                fill-height
+                                align-center
+                                justify-center
+                                ma-0>
+                            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                        </v-layout>
+                    </template>
+                </v-img>
+                <div class="playlist-name pa-3">
+                    <h1 class="display-1 font-weight-light">{{ playlist.name }}</h1>
+                    <h2 class="subheading">{{ playlistType }}</h2>
+                </div>
+            </div>
+        </template>
     </div>
 </template>
 
@@ -29,7 +55,7 @@
             }
         },
         created() {
-            if (!this.cachedPlaylist) {
+            if (!this.playlist) {
                 axios.get(`/api/playlist/${this.id}`)
                     .then(res => this.$store.commit('cache/setSelectedPlaylist', res.data))
                     .catch(err => {
@@ -42,13 +68,20 @@
             }
         },
         computed: {
-            cachedPlaylist() {
+            playlist() {
                 let cachedPlaylist = this.$store.getters['cache/selectedPlaylist'];
                 if (cachedPlaylist && cachedPlaylist.id === parseInt(this.id)) {
                     return cachedPlaylist;
                 }
 
                 return null;
+            },
+            playlistType() {
+                if (this.playlist.spotify_id) {
+                    return 'Spotify Playlist';
+                }
+
+                return 'Playlist';
             }
         }
     }
@@ -68,5 +101,16 @@
         justify-content: space-evenly;
         margin: 0 10px;
         padding: 0 10px;
+    }
+
+    .playlist-image {
+        max-width: 190px;
+    }
+
+    .playlist-name {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        border-bottom: 1px solid rgba(0,0,0,.12);
     }
 </style>

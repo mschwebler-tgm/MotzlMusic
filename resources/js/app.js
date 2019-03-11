1ee4effb09261fa2ab0b71d45c6b76ff44100842
Matthias Schwebler <m.schwebler@laola1.at>
2019-03-11 11:35:28 +0100

    Adds semicolon.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,8 @@ import VueRouter from 'vue-router';
 import router from './router';
 import store from './store/store';
 
-import 'clusterize.js'
-import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import 'clusterize.js';
+import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
 
 import Vue from 'vue';
 import Vuetify from 'vuetify'

80bf7bf930784bc3a3be61dc0bee6248e1971556
Matthias Schwebler <m.schwebler@laola1.at>
2019-03-11 12:23:17 +0100

    Navigates to playlist detail page and puts playlist item in cache.

--- resources/js/components/MyLibrary/Playlists/PlaylistItem.vue
+++ resources/js/components/MyLibrary/Playlists/PlaylistItem.vue
@@ -1,27 +1,25 @@
 <template>
-    <router-link :to="`/playlist/${playlist.name}/${playlist.id}`">
-        <v-card tile>
-            <v-img width="40%"
-                   :src="fullResolutionImage"
-                   :lazy-src="intermediateImage"
-                   :alt="playlist.name"
-                   style="display: inline-block;">
-                <v-layout
-                        slot="placeholder"
-                        fill-height
-                        align-center
-                        justify-center
-                        ma-0>
-                    <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                </v-layout>
-            </v-img>
-            <div style="display: inline-block; top: -10px; position: absolute;">
-                <v-card-title primary-title>
-                    <h3 class="title mb-0">{{ playlist.name }}</h3>
-                </v-card-title>
-            </div>
-        </v-card>
-    </router-link>
+    <v-card tile @click="openPlaylistDetails" class="pointer">
+        <v-img width="40%"
+               :src="fullResolutionImage"
+               :lazy-src="intermediateImage"
+               :alt="playlist.name"
+               style="display: inline-block;">
+            <v-layout
+                    slot="placeholder"
+                    fill-height
+                    align-center
+                    justify-center
+                    ma-0>
+                <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+            </v-layout>
+        </v-img>
+        <div style="display: inline-block; top: -10px; position: absolute;">
+            <v-card-title primary-title>
+                <h3 class="title mb-0">{{ playlist.name }}</h3>
+            </v-card-title>
+        </div>
+    </v-card>
 </template>
 
 <script>
@@ -29,6 +27,12 @@
         name: "PlaylistItem",
         props: {
             playlist: Object,
+        },
+        methods: {
+            openPlaylistDetails() {
+                this.$store.commit('cache/setSelectedPlaylist', this.playlist);
+                this.$router.push(`/playlist/${this.playlist.name}/${this.playlist.id}`);
+            }
         },
         computed: {
             fullResolutionImage() {

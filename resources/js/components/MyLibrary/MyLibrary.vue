43ecbe5b0caf03aa20831cf981c28772a7fdd812
Matthias Schwebler <m.schwebler@laola1.at>
2019-03-12 12:06:19 +0100

    Adds animation to my library components.

--- resources/js/components/MyLibrary/MyLibrary.vue
+++ resources/js/components/MyLibrary/MyLibrary.vue
@@ -1,6 +1,8 @@
 <template>
     <v-card>
-        <router-view></router-view>
+        <transition name="my-library" mode="out-in">
+            <router-view></router-view>
+        </transition>
     </v-card>
 </template>
 
@@ -11,5 +13,33 @@
 </script>
 
 <style scoped>
+    .my-library-enter-active {
+        animation: my-library .2s ease-out;
+    }
 
+    .my-library-leave-active {
+        animation: my-library .1s reverse ease-in;
+    }
+
+    @keyframes my-library {
+        0% {
+            -webkit-transform: translate3d(1rem, 0, 0);
+            -moz-transform: translate3d(1rem, 0, 0);
+            -ms-transform: translate3d(1rem, 0, 0);
+            -o-transform: translate3d(1rem, 0, 0);
+            transform: translate3d(1rem, 0, 0);
+            opacity: 0;
+        }
+        50% {
+            opacity: .8;
+        }
+        100% {
+            -webkit-transform: translate3d(0, 0, 0);
+            -moz-transform: translate3d(0, 0, 0);
+            -ms-transform: translate3d(0, 0, 0);
+            -o-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+            opacity: 1;
+        }
+    }
 </style>

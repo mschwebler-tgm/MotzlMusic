ad81c966ef1044c555f0c7babe0ca650c3d61d09
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-03-17 17:41:38 +0100

    Adds forgot password link.

--- resources/js/components/Auth/Login.vue
+++ resources/js/components/Auth/Login.vue
@@ -7,20 +7,37 @@
             <v-text-field box required
                           id="email"
                           name="email"
-                          label="E-mail">
+                          label="E-mail"
+                          autocomplete="off">
             </v-text-field>
             <v-text-field box required
                           id="password"
                           type="password"
                           name="password"
-                          label="Password">
+                          label="Password"
+                          autocomplete="off">
             </v-text-field>
-            <input type="hidden" name="_token" :value="csrf">
-            <v-btn class="primary" type="submit">Login</v-btn>
-            <v-btn tag="a"
-                   class="primary"
-                   href="/register"
-                   outline>Register</v-btn>
+            <v-checkbox
+                    name="remember"
+                    id="remember"
+                    label="'member me"
+            ></v-checkbox>
+            <v-layout justify-space-between align-center row>
+                <div>
+                    <v-btn class="primary" type="submit">Login</v-btn>
+                    <v-btn tag="a"
+                           class="primary"
+                           href="/register"
+                           outline>Register</v-btn>
+                </div>
+                <v-btn flat right
+                       :ripple="false"
+                       tag="a"
+                       href="/password/reset"
+                       color="primary">
+                    Forgot your password?
+                </v-btn>
+            </v-layout>
         </v-form>
     </div>
 </template>

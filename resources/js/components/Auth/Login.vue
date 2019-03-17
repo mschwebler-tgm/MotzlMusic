d7beaf4ee7f2bbebda9b0fa760bdf55a3d4cd64a
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-03-17 18:28:13 +0100

    Refactor: extracts form to own component.

--- resources/js/components/Auth/Login.vue
+++ resources/js/components/Auth/Login.vue
@@ -3,54 +3,10 @@
         <div class="item elevation-5 pa-4">
             <h1 class="display-2 font-weight-light">Login</h1>
             <v-divider class="mb-3 mt-3"></v-divider>
-            <v-form lazy-validation method="POST" action="/login" id="login-form">
-                <input type="hidden" name="_token" :value="csrf">
-                <v-text-field solo required single-line
-                              id="email"
-                              name="email"
-                              label="E-mail"
-                              :rules="emailRules"
-                              :error-messages="errors.email"
-                              browser-autocomplete="off">
-                </v-text-field>
-                <v-text-field solo required single-line
-                              id="password"
-                              name="password"
-                              label="Password"
-                              browser-autocomplete="off"
-                              :append-icon="showPassword ? 'visibility' : 'visibility_off'"
-                              :type="showPassword ? 'text' : 'password'"
-                              :error-messages="errors.password"
-                              @click:append="showPassword = !showPassword">
-                </v-text-field>
-                <v-checkbox
-                        name="remember"
-                        id="remember"
-                        label="'member me">
-                </v-checkbox>
-                <v-layout justify-space-between align-center row>
-
-                    <v-progress-linear :indeterminate="true" v-show="loading"></v-progress-linear>
-                    <div v-show="!loading">
-                        <v-btn class="primary"
-                               type="submit"
-                               @click="loading = true">Login</v-btn>
-                        <v-btn tag="a"
-                               class="primary"
-                               href="/register"
-                               outline>Register
-                        </v-btn>
-                    </div>
-                    <v-btn v-show="!loading"
-                           flat right
-                           :ripple="false"
-                           tag="a"
-                           href="/password/reset"
-                           color="primary">
-                        Forgot your password?
-                    </v-btn>
-                </v-layout>
-            </v-form>
+            <auth-login-form v-show="!showForgotPassword"
+                             :csrf="csrf"
+                             :errors="errors"
+                             :showForgotPassword.sync="showForgotPassword"></auth-login-form>
         </div>
         <v-layout justify-center row class="mt-4">
             <v-btn flat
@@ -65,17 +21,14 @@
 </template>
 
 <script>
+    import AuthLoginForm from "./LoginForm";
     export default {
-        name: "Login",
+        name: "AuthLogin",
+        components: {AuthLoginForm},
         props: ['csrf', 'errors'],
         data() {
             return {
-                emailRules: [
-                    email => !!email || 'E-mail is required',
-                    email => /.+@.+/.test(email) || 'E-mail must be valid'
-                ],
-                showPassword: false,
-                loading: false,
+                showForgotPassword: false,
             }
         }
     }

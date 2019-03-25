74f19e35e9a1acb4a63d43e8bc9cee45e54b1bbb
Matthias Schwebler <m.schwebler@laola1.at>
2019-03-25 17:28:20 +0100

    Optimizes track table for mobile phones.

--- resources/js/store/modules/myLibrary/helpers/clusterizeTracks.js
+++ resources/js/store/modules/myLibrary/helpers/clusterizeTracks.js
@@ -2,9 +2,9 @@ const clusterizeTracks = function (tracks) {
     return tracks.reduce((rows, track, index) => rows.concat([
         `<div class="track is-unselectable" data-id="${track.id}" tabindex="1">` +
         `<div class="track-list-number">&nbsp;<span>${index + 1}.</span></div>` +
-        `<div class="track-list-title">&nbsp;<span title="${track.name}">${track.name}</span></div>` +
+        `<div class="track-list-title">&nbsp;<span title="${track.name}" class="text-truncate">${track.name}</span></div>` +
         `<div class="track-list-duration">&nbsp;<span>${track.duration_formatted}</span></div>` +
-        `<div class="track-list-artist">&nbsp;<span title="${track.artist.name}">${track.artist.name}</span></div>` +
+        `<div class="track-list-artist">&nbsp;<span title="${track.artist.name}" class="text-truncate">${track.artist.name}</span></div>` +
         `<div class="track-list-rating">&nbsp;Star rating</div>` +
         `<div class="flex-1 track-list-options-trigger"></div>` +
         `<div class="track-list-options-trigger"><span class="icon has-text-grey-light" title="Spotify"><i class="mdi mdi-spotify mdi-24px"></i></span></div>` +

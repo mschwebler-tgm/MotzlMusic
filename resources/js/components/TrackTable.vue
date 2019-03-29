ade1f5e55439a846adecd275187e1a5e5f5a921d
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-03-29 18:30:12 +0100

    Refactor: extracts low level logic to base component.

--- resources/js/components/TrackTable.vue
+++ resources/js/components/TrackTable.vue
@@ -1,81 +1,32 @@
 <template>
-    <v-flex class="track-table" id="my-library-tracks-table">
-        <div :id="identifier + '-scrollArea'"
-             :style="{'max-height': scrollContainerHeight}"
-             class="clusterize-scroll"
-             ref="scrollArea">
-            <div :id="identifier + '-contentArea'" class="clusterize-content">
-                <tr class="clusterize-no-data">
-                </tr>
-            </div>
-        </div>
-        <div class="d-flex justify-center" v-if="showLoading">
-            <v-progress-circular
-                    color="accent"
-                    indeterminate>
-            </v-progress-circular>
-        </div>
-    </v-flex>
+    <base-track-table
+            :tracks="tracks"
+            :renderFunction="renderFunction"
+            :height="height"
+            @track-selected="playTrack">
+    </base-track-table>
 </template>
 
 <script>
-    import Clusterize from "clusterize.js";
+    import BaseTrackTable from "./BaseTrackTable";
 
     export default {
         name: "TrackTable",
+        components: {BaseTrackTable},
         props: {
             tracks: Array,
             renderFunction: Function,
             height: String,
         },
-        data() {
-            return {
-                identifier: Math.random().toString(36).substring(7),
-                scrollContainerHeight: this.height || '500px',
-                isInitialized: false,
-            }
-        },
-        mounted() {
-            if (!this.isInitialized) {
-                this.initializeTracksTable();
-            }
-            this.initDoubleClickListener();
-        },
-        watch: {
-            tracks() {
-                this.initializeTracksTable();
-            }
-        },
         methods: {
-            initializeTracksTable() {
-                this.isInitialized = true;
-                this.clusterize = new Clusterize({
-                    scrollId: this.identifier + '-scrollArea',
-                    contentId: this.identifier + '-contentArea',
-                    rows: this.renderFunction(this.tracks),
-                });
-            },
-            initDoubleClickListener() {
-                const findTrackElement = element => element.classList.contains('track') ? element : findTrackElement(element.parentElement);
-                document.getElementById(this.identifier + '-contentArea').addEventListener('dblclick', event => {
-                    const trackElement = findTrackElement(event.target);
-                    const trackId = trackElement.getAttribute('data-id');
-                    const track = this.tracks.filter(track => track.id === parseInt(trackId))[0];
-                    this.playerController.play(track);
-                });
-            },
+            playTrack(track) {
+                this.playerController.play(track);
+            }
         },
         computed: {
-            showLoading() {
-                return this.tracks.length === 0 || !this.isInitialized;
-            },
             playerController() {
                 return this.$store.getters['player/controller'];
             }
         }
     }
 </script>
-
-<style lang="scss">
-    @import "../../sass/components/_trackTable";
-</style>

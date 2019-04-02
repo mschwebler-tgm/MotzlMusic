a1a7e99c96ed4341f492c3d60052ec9ae4e64b5d
Matthias Schwebler <m.schwebler@laola1.at>
2019-04-02 13:41:34 +0200

    Adds enter animation to track info tile.

--- resources/js/components/SubContent/TrackInfo.vue
+++ resources/js/components/SubContent/TrackInfo.vue
@@ -1,25 +1,27 @@
 <template>
-    <v-card>
-        <div class="d-flex" v-if="this.currentTrack">
-            <v-flex shrink class="pa-0" style="flex: 0 !important;">
-                <img :src="imageSrc" width="150" height="150">
-            </v-flex>
-            <v-flex grow class="d-flex pl-4 justify-center flex-column">
-                <div style="max-width: 180px;">
-                    <div class="text-truncate headline">{{ currentTrack.name }}</div>
-                    <div class="text-truncate">{{ album.name }}</div>
-                    <div>({{ albumDate }})</div>
-                </div>
-                <div class="pt-2">
-                    <v-icon style="font-size: 20px">star</v-icon>
-                    <v-icon style="font-size: 20px">star</v-icon>
-                    <v-icon style="font-size: 20px">star</v-icon>
-                    <v-icon style="font-size: 20px">star_half</v-icon>
-                    <v-icon style="font-size: 20px">star_border</v-icon>
-                </div>
-            </v-flex>
-        </div>
-    </v-card>
+    <transition name="slideFromTop">
+        <v-card v-if="this.currentTrack">
+            <div class="d-flex">
+                <v-flex shrink class="pa-0" style="flex: 0 !important;">
+                    <img :src="imageSrc" width="150" height="150">
+                </v-flex>
+                <v-flex grow class="d-flex pl-4 justify-center flex-column">
+                    <div style="max-width: 180px;">
+                        <div class="text-truncate headline">{{ currentTrack.name }}</div>
+                        <div class="text-truncate">{{ album.name }}</div>
+                        <div>({{ albumDate }})</div>
+                    </div>
+                    <div class="pt-2">
+                        <v-icon style="font-size: 20px">star</v-icon>
+                        <v-icon style="font-size: 20px">star</v-icon>
+                        <v-icon style="font-size: 20px">star</v-icon>
+                        <v-icon style="font-size: 20px">star_half</v-icon>
+                        <v-icon style="font-size: 20px">star_border</v-icon>
+                    </div>
+                </v-flex>
+            </div>
+        </v-card>
+    </transition>
 </template>
 
 <script>
@@ -49,4 +51,34 @@
 
 <style scoped>
 
+    .slideFromTop-enter-active {
+        animation: slideFromTop 1s ease-out;
+    }
+
+    @keyframes slideFromTop {
+        0% {
+            -webkit-transform: translate3d(0, -2rem, 0);
+            -moz-transform: translate3d(0, -2rem, 0);
+            -ms-transform: translate3d(0, -2rem, 0);
+            -o-transform: translate3d(0, -2rem, 0);
+            transform: translate3d(0, -2rem, 0);
+            opacity: 0;
+        }
+        50% {
+            -webkit-transform: translate3d(0, -.5rem, 0);
+            -moz-transform: translate3d(0, -.5rem, 0);
+            -ms-transform: translate3d(0, -.5rem, 0);
+            -o-transform: translate3d(0, -.5rem, 0);
+            transform: translate3d(0, -.5rem, 0);
+            opacity: .8;
+        }
+        100% {
+            -webkit-transform: translate3d(0, 0, 0);
+            -moz-transform: translate3d(0, 0, 0);
+            -ms-transform: translate3d(0, 0, 0);
+            -o-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+            opacity: 1;
+        }
+    }
 </style>

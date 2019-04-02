40b4e5b8b7b5d5c114c190415960414cd5a80d93
Matthias Schwebler <m.schwebler@laola1.at>
2019-04-02 13:35:19 +0200

    Adds data to track info tile.

--- resources/js/components/SubContent/TrackInfo.vue
+++ resources/js/components/SubContent/TrackInfo.vue
@@ -1,14 +1,14 @@
 <template>
     <v-card>
-        <div class="d-flex">
+        <div class="d-flex" v-if="this.currentTrack">
             <v-flex shrink class="pa-0" style="flex: 0 !important;">
                 <img :src="imageSrc" width="150" height="150">
             </v-flex>
             <v-flex grow class="d-flex pl-4 justify-center flex-column">
-                <div>
-                    <div class="headline">Supermodel</div>
-                    <div>Foster the People</div>
-                    <div>(2014)</div>
+                <div style="max-width: 180px;">
+                    <div class="text-truncate headline">{{ currentTrack.name }}</div>
+                    <div class="text-truncate">{{ album.name }}</div>
+                    <div>({{ albumDate }})</div>
                 </div>
                 <div class="pt-2">
                     <v-icon style="font-size: 20px">star</v-icon>
@@ -31,6 +31,17 @@
             },
             imageSrc() {
                 return this.currentTrack ? this.currentTrack.album.spotify_image_medium : null;
+            },
+            album() {
+                return this.currentTrack ? this.currentTrack.album : null;
+            },
+            albumDate() {
+                if (!this.album) {
+                    return;
+                }
+                const date = new Date(this.album.release_date);
+
+                return date.getFullYear();
             }
         }
     }

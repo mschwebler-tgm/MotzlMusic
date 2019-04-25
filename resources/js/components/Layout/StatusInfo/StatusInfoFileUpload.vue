a8cb7f2e3ba493f360f9da8bf703bfecb21d8f6f
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-04-25 21:01:07 +0200

    Updates upload status text when finished.

--- resources/js/components/Layout/StatusInfo/StatusInfoFileUpload.vue
+++ resources/js/components/Layout/StatusInfo/StatusInfoFileUpload.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="d-flex align-center">
-        <div class="caption mr-2 grey--text text--darken-1">Uploading tracks... {{ totalFilesCount - remainingFilesCount }}/{{ totalFilesCount }}</div>
+        <div class="caption mr-2 grey--text text--darken-1">{{ uploadText }} {{ totalFilesCount - remainingFilesCount }}/{{ totalFilesCount }}</div>
         <v-progress-circular :value="progressInPercent"
                              color="primary darken-3"
                              size="20"
@@ -13,6 +13,16 @@
 
     export default {
         name: "StatusInfoFileUpload",
+        data() {
+            return {
+                uploadText: 'Uploading tracks...',
+            }
+        },
+        watch: {
+            uploadInProgress(isInProgress) {
+                this.uploadText = isInProgress ? 'Uploading tracks...' : 'Upload done';
+            }
+        },
         computed: {
             ...mapGetters('fileUpload', [
                 'files',

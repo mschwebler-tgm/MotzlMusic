8fb2bae8a0f79ee055349a547ecf124deee5c61e
Matthias Schwebler <m.schwebler@laola1.at>
2019-05-01 20:18:58 +0200

    Adds empty state for tracks.

--- resources/js/components/MyLibrary/Tracks/Tracks.vue
+++ resources/js/components/MyLibrary/Tracks/Tracks.vue
@@ -2,7 +2,16 @@
     <div>
         <v-card-title class="headline">Tracks</v-card-title>
         <v-container>
-            <v-layout column class="clusterize">
+            <div v-if="noPlaylistsAvailable">
+                <div class="d-flex align-center flex-column">
+                    <p class="subheading">You have no playlists in your library</p>
+                    <div>
+                        <v-btn color="primary" to="/upload">upload some!</v-btn>
+                        <v-btn flat color="primary" to="/import/spotify">Import from spotify</v-btn>
+                    </div>
+                </div>
+            </div>
+            <v-layout column class="clusterize" v-else>
                 <track-table :tracks="tracks" :render-function="renderFunction" height="612px"></track-table>
             </v-layout>
         </v-container>
@@ -12,6 +21,7 @@
 <script>
     import TrackTable from "../../TrackTable";
     import renderFunction from '../../../store/modules/myLibrary/helpers/clusterizeTracks';
+    import {mapGetters} from "vuex";
 
     export default {
         name: "Tracks",
@@ -22,8 +32,12 @@
             }
         },
         computed: {
-            tracks() {
-                return this.$store.getters['myLibrary/tracks'];
+            ...mapGetters('myLibrary', [
+                'tracks',
+                'tracksInitialized',
+            ]),
+            noPlaylistsAvailable() {
+                return this.tracksInitialized && this.tracks.length === 0;
             },
         }
     }

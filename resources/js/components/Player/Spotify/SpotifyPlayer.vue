dd1d4ad2e991df87ce266e52be151b00ecd925a9
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-05-23 21:50:39 +0200

    Removes logging.

--- resources/js/components/Player/Spotify/SpotifyPlayer.vue
+++ resources/js/components/Player/Spotify/SpotifyPlayer.vue
@@ -48,17 +48,14 @@
                             }
 
                             this.$store.getters['player/controller'].spotifyPlayer.deviceId = device_id;
-                            console.log('Ready with Device ID', device_id);
                         });
 
                         // Not Ready
                         player.addListener('not_ready', ({device_id}) => {
-                            console.log('Device ID has gone offline', device_id);
                         });
 
                         player.connect();
                         this.player = player;
-                        console.log(player);
                         this.$store.commit('player/setSpotifyPlayer', this.player);
                     });
                 }
--- resources/js/store/modules/player/actions.js
+++ resources/js/store/modules/player/actions.js
@@ -2,14 +2,14 @@ export default {
     init() {
     },
     play({state}, track) {
-        state.playerController.play(track);
+        return state.playerController.play(track);
     },
     pause({state}) {
         state.playerController.pause();
     },
     playNext({state, dispatch}) {
         state.queueController.setNext();
-        dispatch('play', state.queueController.currentTrack);
+        return dispatch('play', state.queueController.currentTrack);
     },
     playPrevious({state, dispatch}) {
         state.queueController.setPrevious();

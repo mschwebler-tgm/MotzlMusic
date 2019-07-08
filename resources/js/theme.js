bca3ccc548a1caf0f1c0d3578d1f7c05ac303409
Matthias Schwebler <m.schwebler@laola1.at>
2019-07-08 14:24:59 +0200

    Adjusts colors.

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,9 +1,12 @@
-import colors from "vuetify/es5/util/colors";
+// import colors from "vuetify/es5/util/colors";
 
 const theme = {
-    primary: colors.blue.lighten1,
-    secondary: colors.lime.base,
-    accent: colors.blue.accent2,
+    primary: '#42A4F5',
+    secondary: '#92D050',
+    accent: '#1D69A8',
+    success: '#92D050',
+    warning: '#A3A825',
+    error: '#F7615B'
 };
 
 const setSystemBarColor = function (color) {

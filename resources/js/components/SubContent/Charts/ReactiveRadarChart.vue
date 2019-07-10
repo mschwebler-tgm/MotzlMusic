f6437411b438ee7f5ee8ee502ca63d96f25f9008
Matthias Schwebler <m.schwebler@laola1.at>
2019-07-10 16:40:49 +0200

    Creates charts with values from properties.

--- resources/js/components/SubContent/Charts/ReactiveRadarChart.vue
+++ resources/js/components/SubContent/Charts/ReactiveRadarChart.vue
@@ -5,8 +5,32 @@
 <script>
     import radarChartConfig from "./radarChartConfig";
 
+    const seriesData = {
+        type: Array,
+        default: () => [null, null, null, null, null, null],
+        validator: arr => arr.length === 6,
+    };
+
     export default {
         name: "ReactiveRadarChart",
+        props: {
+            primaryData: {...seriesData},
+            secondaryData: {...seriesData},
+            primaryLabel: {
+                type: String,
+                default: 'Selected tracks (avg)',
+            },
+            secondaryLabel: {
+                type: String,
+                default: 'Playing track',
+            },
+        },
+        created() {
+            radarChartConfig.series[0].data = this.secondaryData;
+            radarChartConfig.series[0].name = this.secondaryLabel;
+            radarChartConfig.series[1].data = this.primaryData;
+            radarChartConfig.series[1].name = this.primaryLabel;
+        },
         mounted() {
             Highcharts.chart('chart', radarChartConfig);
         }

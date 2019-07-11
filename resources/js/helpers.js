cbdaa84f18efeebbbc3a701c5f42da3c767e7236
Matthias Schwebler <m.schwebler@laola1.at>
2019-07-11 14:06:14 +0200

    Optimizes slug function.

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -1,10 +1,9 @@
 function slugify(text) {
-    return text.toString().toLowerCase()
+    return text.toString().toLowerCase().trim()
         .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/&/g, '-and-')         // Replace & with 'and'
         .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-        .replace(/^-+/, '')             // Trim - from start of text
-        .replace(/-+$/, '');            // Trim - from end of text
+        .replace(/\-\-+/g, '-');        // Replace multiple - with single -
 }
 
 export {slugify};

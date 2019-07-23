4fb5b36bf870d55b52b6bde3dcf8f6ab8767c064
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-07-23 21:13:01 +0200

    Adds notification settings content.

--- resources/js/components/Settings/Categories/Notifications.vue
+++ resources/js/components/Settings/Categories/Notifications.vue
@@ -1,10 +1,32 @@
 <template>
-    
+    <div>
+        <h3 class="headline">Receive Notifications</h3>
+        <v-divider class="mb-3 mt-3"></v-divider>
+        <v-switch v-model="settings.uploads" label="New tracks available (from other users)"></v-switch>
+        <v-switch v-model="settings.messages" label="Chat messages"></v-switch>
+        <v-switch v-model="settings.concerts" label="Nearby concert announcements"></v-switch>
+        <v-divider class="mb-3 mt-3"></v-divider>
+        <v-btn color="accent" @click="save">Save</v-btn>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Notifications"
+        name: "Notifications",
+        data() {
+            return {
+                settings: {
+                    uploads: true,
+                    messages: true,
+                    concerts: true,
+                }
+            }
+        },
+        methods: {
+            save() {
+                alert('Coming soon');
+            }
+        },
     }
 </script>
 

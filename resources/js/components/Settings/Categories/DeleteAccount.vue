73578f8e8723160337a7b6bbcce86078c33f789c
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-07-23 21:46:36 +0200

    Adds delete account settings content.

--- resources/js/components/Settings/Categories/DeleteAccount.vue
+++ resources/js/components/Settings/Categories/DeleteAccount.vue
@@ -1,5 +1,10 @@
 <template>
-    
+    <div>
+        <h3 class="headline">Delete your account</h3>
+        <v-divider class="mb-3 mt-3"></v-divider>
+        <v-text-field label="Confirm your password" type="password" box class="password"></v-text-field>
+        <v-btn class="error">Delete account</v-btn>
+    </div>
 </template>
 
 <script>
@@ -9,5 +14,7 @@
 </script>
 
 <style scoped>
-
+    .password {
+        max-width: 250px;
+    }
 </style>

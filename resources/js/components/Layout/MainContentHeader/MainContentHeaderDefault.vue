eeebfa01fab48ffa81b76a4ef779c9ef328003d5
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-07-23 21:57:37 +0200

    Fixes back button by removing functional flag from component.

--- resources/js/components/Layout/MainContentHeader/MainContentHeaderDefault.vue
+++ resources/js/components/Layout/MainContentHeader/MainContentHeaderDefault.vue
@@ -1,4 +1,4 @@
-<template functional>
+<template>
     <div class="justify-space-between pa-0" style="display: flex;">
         <v-btn flat @click="$router.go(-1)" class="ml-0" style="flex: 0 !important;">
             <v-icon dark left>arrow_back</v-icon>

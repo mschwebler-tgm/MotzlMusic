45d305c4690ea0155f078e2e191e028b341569ca
Matthias Schwebler <m.schwebler@laola1.at>
2019-07-24 13:09:39 +0200

    Refactor: extracts closure route to controller to enable route caching.

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', 'HomeController@index')->name('home');
 Auth::routes();
 
-Route::get('/{any}', function () {
-    $user = Auth::user();
-    if (!$user) {
-        return redirect('/login');
-    }
-    return view('master')->with(['user' => $user]);
-})->where('any', '^(?!api\/|storage\/).+')->name('app');
+Route::get('/{any}', 'HomeController@app')
+    ->where('any', '^(?!api\/|storage\/).+')
+    ->name('app');

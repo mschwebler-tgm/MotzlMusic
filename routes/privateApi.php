7e0c496715ff058fa4e6255abb655a830c9f6c72
Matthias Schwebler <m.schwebler@laola1.at>
2019-07-24 13:06:48 +0200

    Removes unused routes.

--- routes/privateApi.php
+++ routes/privateApi.php
@@ -1,11 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-});
 
 # Spotify
 Route::prefix('spotify')->namespace('Spotify')->group(function () {

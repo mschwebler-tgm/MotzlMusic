365b58d1138062f0710924c15101912773534913
Matthias Schwebler <m.schwebler@laola1.at>
2019-07-25 14:45:13 +0200

    Adds alerts on save.

--- resources/js/components/Settings/Categories/Notifications.vue
+++ resources/js/components/Settings/Categories/Notifications.vue
@@ -2,11 +2,17 @@
     <div>
         <h3 class="headline">Receive Notifications</h3>
         <v-divider class="mb-3 mt-3"></v-divider>
+        <v-alert type="success" border="left" v-show="success">
+            Notification settings updated!
+        </v-alert>
+        <v-alert type="error" border="left" v-show="error">
+            {{ error }}
+        </v-alert>
         <v-switch v-model="settings.uploads" label="New tracks available (from other users)"></v-switch>
         <v-switch v-model="settings.messages" label="Chat messages"></v-switch>
         <v-switch v-model="settings.concerts" label="Nearby concert announcements"></v-switch>
         <v-divider class="mb-3 mt-3"></v-divider>
-        <v-btn color="accent" @click="save">Save</v-btn>
+        <v-btn color="accent" @click="save" :loading="loading">Save</v-btn>
     </div>
 </template>
 
@@ -15,16 +21,22 @@
         name: "Notifications",
         data() {
             return {
-                settings: {
-                    uploads: true,
-                    messages: true,
-                    concerts: true,
-                }
+                settings: {...this.$root.user.settings.notifications},
+                loading: false,
+                success: false,
+                error: false,
             }
         },
         methods: {
             save() {
-                axios.post('/api/settings/notifications', this.settings);
+                this.loading = true;
+                axios.post('/api/settings/notifications', this.settings)
+                    .then(() => {
+                        this.success = true;
+                        setTimeout(() => this.success = false, 5000);
+                    })
+                    .catch(err => this.error = err.message)
+                    .finally(() => this.loading = false);
             }
         },
     }

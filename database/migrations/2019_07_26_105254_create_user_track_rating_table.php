d188b4053ea1143a8dc219566cd6e44ae0052f70
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2019-07-26 12:55:02 +0200

    Creates table for user track rating.

---
+++ database/migrations/2019_07_26_105254_create_user_track_rating_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTrackRatingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_track_rating', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('track_id');
+            $table->unsignedSmallInteger('rating');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('track_id')->references('id')->on('tracks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_track_rating');
+    }
+}

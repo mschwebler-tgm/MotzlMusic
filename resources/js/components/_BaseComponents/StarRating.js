b28809c2b296dbcecd58d76585f362ff3972464c
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-08-07 20:04:00 +0200

    Adds option to fill remaining empty stars.

--- resources/js/components/_BaseComponents/StarRating.js
+++ resources/js/components/_BaseComponents/StarRating.js
@@ -1,13 +1,19 @@
-import {starFull, starHalf} from "../../store/modules/myLibrary/helpers/starsSVGs";
+import {starEmpty, starFull, starHalf} from "../../store/modules/myLibrary/helpers/starsSVGs";
 
 export default class StarRating {
-    static getStarSVGs(amount) {
+    static getStarSVGs(amount, fillWithEmptyStars = false) {
         const stars = [];
         for (let i = 0; i < Math.floor(amount); i++) {
             stars.push(starFull);
         }
         if (amount % 1 === 0.5) {
             stars.push(starHalf);
+        }
+        if (fillWithEmptyStars) {
+            const remainingStars = 5 - stars.length;
+            for (let i = 0; i < remainingStars; i++) {
+                stars.push(starEmpty);
+            }
         }
 
         return stars;

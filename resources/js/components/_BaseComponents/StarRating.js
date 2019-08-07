6e7a80d395429221c6991ddcc9b09f672dcfc972
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-08-07 20:36:51 +0200

    Emits event when hover rating changes and on click input.

--- resources/js/components/_BaseComponents/StarRating.js
+++ resources/js/components/_BaseComponents/StarRating.js
@@ -4,7 +4,45 @@ export default class StarRating {
 
     constructor(wrapperElement) {
         this._wrapperElement = wrapperElement;
-        this.initMouseMoveListener();
+        this._previousStarsAmount = null;
+        this._onRateCallback = () => console.warn('[warning] no callback for star rating input defined');
+        this._onRateHoverCallback = () => console.warn('[warning] no callback for star rating hover defined');
+        this.initMouseEventListeners();
+    }
+
+    initMouseEventListeners() {
+        this._wrapperElement.addEventListener('mousemove', $event => {
+            this._handleMouseMove($event);
+        });
+        this._wrapperElement.addEventListener('click', () => this._handleMouseClick());
+    }
+
+    _handleMouseMove($event) {
+        const starsAmount = this._calculateStarsAmountForMousePosition($event);
+        if (starsAmount !== this._previousStarsAmount) {
+            this._previousStarsAmount = starsAmount;
+            this._onRateHoverCallback(starsAmount);
+        }
+    }
+
+    _calculateStarsAmountForMousePosition($event) {
+        const starWidth = 24;
+        const offsetLeft = $event.pageX - this._wrapperElement.getBoundingClientRect().x;
+        const halfStarsAmount = Math.ceil(offsetLeft / (starWidth / 2));
+
+        return halfStarsAmount / 2;
+    }
+
+    _handleMouseClick() {
+        this._onRateCallback(this._previousStarsAmount);
+    }
+
+    onRateHover(callback) {
+        this._onRateHoverCallback = callback;
+    }
+
+    onRate(callback) {
+        this._onRateCallback = callback;
     }
 
     static getStarSVGs(amount, fillWithEmptyStars = false) {
@@ -23,24 +61,5 @@ export default class StarRating {
         }
 
         return stars;
-    }
-
-    initMouseMoveListener() {
-        this._wrapperElement.addEventListener('mousemove', $event => {
-            this._handleMouseMove($event);
-        })
-    }
-
-    _handleMouseMove($event) {
-        const starsAmount = this._calculateStarsAmountForMousePosition($event);
-        console.log(starsAmount);
-    }
-
-    _calculateStarsAmountForMousePosition($event) {
-        const starWidth = 24;
-        const offsetLeft = $event.pageX - this._wrapperElement.getBoundingClientRect().x;
-        const halfStarsAmount = Math.ceil(offsetLeft / (starWidth / 2));
-
-        return halfStarsAmount / 2;
     }
 };

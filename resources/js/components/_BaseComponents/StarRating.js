8599fd1cd1cce49f003d84108d7486fa8e53f31f
Matthias Schwebler <matthias.schwebler1998@gmail.com>
2019-08-07 20:28:08 +0200

    Calculates stars based on current mouse position.

--- resources/js/components/_BaseComponents/StarRating.js
+++ resources/js/components/_BaseComponents/StarRating.js
@@ -1,6 +1,12 @@
 import {starEmpty, starFull, starHalf} from "../../store/modules/myLibrary/helpers/starsSVGs";
 
 export default class StarRating {
+
+    constructor(wrapperElement) {
+        this._wrapperElement = wrapperElement;
+        this.initMouseMoveListener();
+    }
+
     static getStarSVGs(amount, fillWithEmptyStars = false) {
         const stars = [];
         for (let i = 0; i < Math.floor(amount); i++) {
@@ -17,5 +23,24 @@ export default class StarRating {
         }
 
         return stars;
+    }
+
+    initMouseMoveListener() {
+        this._wrapperElement.addEventListener('mousemove', $event => {
+            this._handleMouseMove($event);
+        })
+    }
+
+    _handleMouseMove($event) {
+        const starsAmount = this._calculateStarsAmountForMousePosition($event);
+        console.log(starsAmount);
+    }
+
+    _calculateStarsAmountForMousePosition($event) {
+        const starWidth = 24;
+        const offsetLeft = $event.pageX - this._wrapperElement.getBoundingClientRect().x;
+        const halfStarsAmount = Math.ceil(offsetLeft / (starWidth / 2));
+
+        return halfStarsAmount / 2;
     }
 };

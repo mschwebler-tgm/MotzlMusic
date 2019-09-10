d52b69978d6eda3b1321f68b0cd137d2e0bf542c
Matthias Schwebler <m.schwebler@laola1.at>
2019-09-10 13:31:41 +0200

    Player v2: removes unused code.

--- resources/js/components/Player/Player.vue
+++ resources/js/components/Player/Player.vue
@@ -6,30 +6,10 @@
 
 <script>
     import SpotifyPlayer from "./Spotify/SpotifyPlayer";
-    import PlayerController from "../../store/modules/player/helpers/PlayerController";
 
     export default {
         name: "Player",
         components: {SpotifyPlayer},
-        created() {
-            const playerController = new PlayerController(this.$store);
-            this.$store.commit('player/setPlayerController', playerController);
-            this.initListeners();
-        },
-        methods: {
-            initListeners() {
-                this.handleProgress();
-            },
-            handleProgress() {
-                this.$store.watch(
-                    $store => $store.player.playerController.progressPercent,
-                    progressPercent => {
-                        if (progressPercent >= 100) {
-                            this.$store.dispatch('player/playNext');
-                        }
-                    });
-            },
-        }
     }
 </script>
 
--- resources/js/store/modules/player/actions.js
+++ resources/js/store/modules/player/actions.js
@@ -1,72 +1,9 @@
 export default {
     init() {
     },
-    play({state}, track) {
-        // TODO validateTrack: play track before validating? load artist and album afterwards?
-        updateActiveTrackClass(track, state.playerController.playingTrack);
-        validateTrack(track).then(() => {
-            return state.playerController.play(track);
-        });
-    },
-    pause({state}) {
-        state.playerController.pause();
-    },
-    playQueue({state, dispatch}, trackList) {
-        if (trackList.length === 0) {
-            return;
-        }
-
-        state.queueController.setQueue(trackList);
-        dispatch('play', trackList[0]);
-    },
-    playNext({state, dispatch}) {
-        state.queueController.setNext();
-        return dispatch('play', state.queueController.currentTrack);
-    },
-    playPrevious({state, dispatch}) {
-        state.queueController.setPrevious();
-        dispatch('play', state.queueController.currentTrack);
-    },
-    playList({state, dispatch}, {type, list}) {
-        state.playerController.loading = true;
-        state.activeItem = {type, id: list.id};
-        if (!list.tracks) {
-            axios.get(`/api/${type}/${list.id}/tracks`)
-                .then(res => list.tracks = res.data)
-                .then(() => dispatch('playQueue', list.tracks));
-        } else {
-            dispatch('playQueue', list.tracks);
-        }
-    },
     addSelectedToQueue({rootState, commit}) {
         const focusedItems = rootState.subContent.focusedItems;
         const focusedTracks = focusedItems.filter(item => item.type === 'track');  // only allow tracks to be queued for now
         focusedTracks.forEach(track => commit('addTrackToQueue', track));
     },
-}
-
-function validateTrack(track) {
-    return new Promise((resolve, reject) => {
-        if (!track) {
-            resolve();  // playback will be resumed
-        }
-        axios.get(`/api/track/${track.id}`)
-            .then(res => {
-                track.album = res.data.album;
-                track.artists = res.data.artists;
-            })
-            .then(resolve)
-            .catch(reject);
-    });
-}
-
-function updateActiveTrackClass(newTrack, oldTrack) {
-    if (oldTrack) {
-        const trackRows = [...document.getElementsByClassName(`track-row-${oldTrack.id}`)];
-        trackRows.forEach($row => $row.classList.remove('active'));
-    }
-    if (newTrack) {
-        const trackRows = [...document.getElementsByClassName(`track-row-${newTrack.id}`)];
-        trackRows.forEach($row => $row.classList.add('active'));
-    }
 }

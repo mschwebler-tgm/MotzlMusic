2b2db31d22463192efd86963e40b7f08d392d6ac
Matthias Schwebler <m.schwebler@laola1.at>
2019-09-12 16:55:46 +0200

    Bottom player: adds progress bar.

--- resources/js/components/Player/Player.vue
+++ resources/js/components/Player/Player.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div class="player-wrapper">
+        <v-slider color="secondary" class="progress-bar" hide-details></v-slider>
         <spotify-player></spotify-player>
     </div>
 </template>
@@ -13,6 +14,24 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss">
+    .player-wrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: flex;
+        width: 100%;
+        height: 100%;
 
+        .progress-bar {
+            position: absolute;
+            top: calc(-50% + 2px);
+            left: 0;
+            width: 100%;
+
+            .v-slider {
+                margin: 0;
+            }
+        }
+    }
 </style>

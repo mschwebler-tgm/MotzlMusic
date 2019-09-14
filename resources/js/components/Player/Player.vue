35b8cd91c4c9fc250e56429e367aa74fc3a05cc2
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2019-09-14 15:40:32 +0200

    Bottom player: Adds player controls.

--- resources/js/components/Player/Player.vue
+++ resources/js/components/Player/Player.vue
@@ -4,6 +4,20 @@
                   color="secondary"
                   v-model="progress"
                   :max="trackDuration"></v-slider>
+        <div class="player-content">
+            <div class="player-controls">
+                <v-btn text icon aria-label="Play previous" :disabled="noPreviousTrack">
+                    <v-icon @click="playPrevious">skip_previous</v-icon>
+                </v-btn>
+                <v-btn text icon :loading="loading" @click="togglePlay" :aria-label="playing ? 'Pause' : 'Play'">
+                    <v-icon v-if="playing">pause_circle_filled</v-icon>
+                    <v-icon v-else>play_circle_filled</v-icon>
+                </v-btn>
+                <v-btn text icon aria-label="Play next" :disabled="noNextTrack">
+                    <v-icon @click="playNext">skip_next</v-icon>
+                </v-btn>
+            </div>
+        </div>
         <spotify-player></spotify-player>
     </div>
 </template>
@@ -28,6 +42,15 @@
             trackDuration() {
                 return player.currentTrack ? player.currentTrack.duration : 0;
             },
+            noNextTrack() {
+                return !player.canPlayNext;
+            },
+            noPreviousTrack() {
+                return !player.canPlayPrevious;
+            },
+            loading() {
+                return player.isLoading;
+            },
             progress: {
                 get() {
                     return this.progressMs
@@ -51,13 +74,29 @@
         methods: {
             setProgressInterval() {
                 this.progressInterval = setInterval(() => this.progressMs += 100, 100);
-            }
+            },
+            togglePlay() {
+                if (this.playing) {
+                    player.pause();
+                } else {
+                    player.resume();
+                }
+            },
+            playNext() {
+                player.playNext();
+            },
+            playPrevious() {
+                player.playPrevious();
+            },
         }
     }
 </script>
 
 <style lang="scss">
     .player-wrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
         width: 100%;
         height: 100%;
 
@@ -67,6 +106,10 @@
             top: calc(-50% + 2px);
             left: 0;
             width: 100%;
+        }
+
+        .player-content {
+            display: flex;
         }
     }
 </style>

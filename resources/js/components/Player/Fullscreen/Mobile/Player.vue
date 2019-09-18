e0abad4ae583fa3286c6d6a05050c633d9ec4b0a
Matthias Schwebler <m.schwebler@laola1.at>
2019-09-18 17:18:22 +0200

    Bottom player: adds animated gradient as background.

--- resources/js/components/Player/Fullscreen/Mobile/Player.vue
+++ resources/js/components/Player/Fullscreen/Mobile/Player.vue
@@ -1,6 +1,7 @@
 <template>
     <v-expand-transition>
         <div class="fullscreen-player grey darken-4" v-show="show">
+            <div class="fullscreen-player-gradient"></div>
             <v-container>
                 <div class="fullscreen-player-header">
                     <v-btn text icon @click="$emit('update:show', false)">
@@ -32,10 +33,50 @@
         width: 100vw;
         height: 100vh;
 
+        &-gradient {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100vw;
+            height: 100vh;
+            background: linear-gradient(0deg, rgba(0,0,0, 0) 0%, rgba(50, 175, 170, 0.1) 20%, rgba(50, 175, 170, 0.6) 73%, #0065a8 100%);
+
+            &::before {
+                content: "";
+                position: absolute;
+                top: -50vw;
+                left: -50vw;
+                width: 200vw;
+                height: 160vw;
+                background: linear-gradient(315deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 40%, rgba(50, 175, 170, 0.6) 100%);
+                animation: gradient-animation alternate-reverse 7s infinite ease-in-out;
+            }
+
+            &::after {
+                content: "";
+                position: absolute;
+                top: -50vw;
+                left: -50vw;
+                width: 200vw;
+                height: 160vw;
+                background: linear-gradient(315deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 40%, #0065a8 100%);
+                animation: gradient-animation alternate 5s infinite ease-in-out;
+            }
+        }
+
         &-header {
             display: flex;
             justify-content: space-between;
             align-items: center;
+        }
+    }
+
+    @keyframes gradient-animation {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(95deg);
         }
     }
 </style>

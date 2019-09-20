4537f993a8186c114afe6fbd5e2c4be9e993a501
Matthias Schwebler <m.schwebler@laola1.at>
2019-09-20 19:14:19 +0200

    Performance: moves player mixin calculations to store for better caching.

--- resources/js/store/modules/player/getters.js
+++ resources/js/store/modules/player/getters.js
@@ -1,2 +1,43 @@
+import player from "$store/player/helpers/v2/player";
+import app from "$components/app";
+
 export default {
+    currentTrack() {
+        console.log('evaluated current track');
+        return player.currentTrack;
+    },
+    playing() {
+        return player.isPlaying;
+    },
+    trackDuration() {
+        return player.currentTrack ? player.currentTrack.duration : 0;
+    },
+    noNextTrack() {
+        return !player.canPlayNext;
+    },
+    noPreviousTrack() {
+        return !player.canPlayPrevious;
+    },
+    loading() {
+        return player.isLoading;
+    },
+    title() {
+        return player.currentTrack ? player.currentTrack.name : 'No track';
+    },
+    artists() {
+        return player.currentTrack ? player.currentTrack.artists.map(artist => artist.name).join(', ') : '-';
+    },
+    albumCover() {
+        if (!player.currentTrack || !player.currentTrack.album) {
+            return window.playlistFallback;
+        }
+
+        return app.$root.getSpotifyImage(player.currentTrack.album, 'small');
+    },
+    playerProgress() {
+        return player.progress;
+    },
+    durationFormatted() {
+        return player.currentTrack ? player.currentTrack.duration_formatted : '-:-'
+    },
 }

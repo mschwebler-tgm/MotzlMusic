105e2ca70c6cbf91560b43bfca0ba4a28fd6bb09
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2019-09-27 19:06:26 +0200

    Decreases column size to maximum.

--- database/migrations/2019_09_27_154808_add_sub_content_configuration_to_users_table.php
+++ database/migrations/2019_09_27_154808_add_sub_content_configuration_to_users_table.php
@@ -14,7 +14,7 @@ class AddSubContentConfigurationToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('sub_content', 16384)->nullable();
+            $table->string('sub_content', 16383)->nullable();
         });
     }
 

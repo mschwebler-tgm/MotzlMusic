3bc5c0ddb493caa7b7ede2e4b18739885a6ad626
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2019-09-27 18:31:32 +0200

    Increases size of sub content column.

--- database/migrations/2019_09_27_154808_add_sub_content_configuration_to_users_table.php
+++ database/migrations/2019_09_27_154808_add_sub_content_configuration_to_users_table.php
@@ -14,7 +14,7 @@ class AddSubContentConfigurationToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('sub_content', 4096)->nullable();
+            $table->string('sub_content', 16384)->nullable();
         });
     }
 

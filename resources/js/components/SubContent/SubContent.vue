f9ed1253e1823f2ef1e509ff5c1e7e8390736527
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2019-09-27 17:13:54 +0200

    Makes sub content movable.

--- resources/js/components/SubContent/SubContent.vue
+++ resources/js/components/SubContent/SubContent.vue
@@ -18,15 +18,17 @@
         </v-flex>
         <v-flex grow fill-height>
             <v-layout column class="fill-height">
-                <v-flex v-for="(content, index) in subContent"
-                        :key="content.component"
-                        shrink
-                        :class="{'pt-0': index === 0}">
-                    <v-card>
-                        <component :is="content.component"></component>
-                        <v-overlay :value="$root.subContentEditModeActive" absolute color="primary"></v-overlay>
-                    </v-card>
-                </v-flex>
+                <Container @drop="moveContent">
+                    <Draggable v-for="(content, index) in subContent"
+                               :key="content.component">
+                        <v-flex shrink :class="{'pt-0': index === 0}">
+                            <v-card>
+                                <component :is="content.component"></component>
+                                <v-overlay :value="$root.subContentEditModeActive" absolute color="primary"></v-overlay>
+                            </v-card>
+                        </v-flex>
+                    </Draggable>
+                </Container>
             </v-layout>
         </v-flex>
     </v-layout>
@@ -36,10 +38,11 @@
     import AudioFeatures from "./Components/AudioFeatures";
     import PlayerControls from "./Components/PlayerControls";
     import TrackInfo from "./Components/TrackInfo";
+    import {Container, Draggable} from "vue-smooth-dnd";
 
     export default {
         name: "SubContent",
-        components: {TrackInfo, PlayerControls, AudioFeatures},
+        components: {TrackInfo, PlayerControls, AudioFeatures, Container, Draggable},
         data() {
             return {
                 subContent: [
@@ -55,6 +58,20 @@
                 ],
             }
         },
+        methods: {
+            moveContent(dropResult) {
+                this.arrayMove(this.subContent, dropResult.removedIndex, dropResult.addedIndex);
+            },
+            arrayMove(array, oldIndex, newIndex) {
+                if (newIndex >= array.length) {
+                    let k = newIndex - array.length + 1;
+                    while (k--) {
+                        array.push(undefined);
+                    }
+                }
+                array.splice(newIndex, 0, array.splice(oldIndex, 1)[0]);
+            },
+        }
     }
 </script>
 

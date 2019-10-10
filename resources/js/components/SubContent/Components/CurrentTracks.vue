e1ab5318ba3b8d25f243fcc1593c608ccc7fdcea
Matthias Schwebler <m.schwebler@laola1.at>
2019-10-10 14:25:13 +0200

    Shows list of current tracks being played.

--- resources/js/components/SubContent/Components/CurrentTracks.vue
+++ resources/js/components/SubContent/Components/CurrentTracks.vue
@@ -1,18 +1,23 @@
 <template>
     <div class="pa-3">
-        <v-simple-table dense height="300px" class="current-tracks-table">
-            <tbody>
-                <tr v-for="track in tracks" :key="track.id">
-                    <td></td>
-                    <td>
-                        <div class="track-title text-truncate">
-                        {{ track.name }}
+        <div class="current-tracks">
+            <v-fade-transition>
+                <div class="d-flex align-center justify-center h-100" v-if="tracksRaw.length === 0">
+                    <span>Start playback to see a list of current tracks</span>
+                </div>
+                <div v-else>
+                    <div v-for="track in tracksRaw"
+                         :key="`${track.trackData.id}_${track.isQueued}`"
+                         class="track-row font-weight-light">
+                        <div class="playing-icon">
+                            <img src="/images/icons/volume_up.png" alt="playing" v-if="currentTrackId === track.trackData.id">
                         </div>
-                    </td>
-                    <td class="text-truncate">{{ track.artists[0].name }}</td>
-                </tr>
-            </tbody>
-        </v-simple-table>
+                        <div class="track-title text-truncate">{{ track.trackData.name }}</div>
+                        <div class="track-artist text-truncate">{{ track.trackData.artists[0].name }}</div>
+                    </div>
+                </div>
+            </v-fade-transition>
+        </div>
     </div>
 </template>
 
@@ -20,21 +25,56 @@
     export default {
         name: "CurrentTracks",
         computed: {
-            tracks() {
-                return this.$store.getters['player/trackList'];
+            tracksRaw() {
+                return this.$store.getters['player/trackListRaw'];
+            },
+            currentTrackId() {
+                return this.$store.getters['player/currentTrackId'];
             }
-        }
+        },
     }
 </script>
 
-<style lang="scss">
-    .current-tracks-table {
-        .v-data-table__wrapper::-webkit-scrollbar {
+<style scoped lang="scss">
+    .current-tracks {
+        height: 360px;
+        overflow-y: scroll;
+
+        &::-webkit-scrollbar {
             display: none;
         }
 
-        .track-title {
-            width: 100px;
+        .track-row {
+            display: flex;
+
+            &:nth-child(odd) {
+                background-color: rgba(255, 255, 255, 0.04);
+            }
+
+            &:hover {
+                background-color: rgba(255, 255, 255, 0.11);
+            }
+
+            .playing-icon {
+                width: 25px;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+                > img {
+                    filter: invert(100%);
+                }
+            }
+
+            .track-title {
+                flex: 1;
+                padding-left: 3px;
+                padding-right: 15px;
+            }
+
+            .track-artist {
+                width: 150px;
+            }
         }
     }
 </style>
--- resources/js/store/modules/player/getters.js
+++ resources/js/store/modules/player/getters.js
@@ -65,4 +65,7 @@ export default {
     trackList() {
         return player.trackList;
     },
+    trackListRaw() {
+        return player.trackListRaw;
+    },
 }

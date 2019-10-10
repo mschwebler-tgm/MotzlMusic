ff2a01aff0456754b7864372df4b3009e2d9020f
Matthias Schwebler <m.schwebler@laola1.at>
2019-10-10 16:21:41 +0200

    Clusterize new: initializes clusterize js.

--- resources/js/components/TrackTable/BaseTrackTable2.vue
+++ resources/js/components/TrackTable/BaseTrackTable2.vue
@@ -1,15 +1,68 @@
 <template>
     <div>
-        <div class="clusterize-scroll">
-            <div class="clusterize-content">
+        <div :id="scrollId"
+             class="clusterize-scroll">
+            <div :id="contentId"
+                 class="clusterize-content">
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Clusterize from "clusterize.js";
+    import DesktopClusterizer from "$scripts/components/TrackTable/Clusterizer/Desktop/DesktopClusterizer";
+    import {columns} from "$scripts/components/TrackTable/Clusterizer/Desktop/columns";
+
     export default {
-        name: "BaseTrackTable2"
+        name: "BaseTrackTable2",
+        props: {
+            tracks: Array,
+        },
+        data() {
+            return {
+                tableId: 'table' + Math.random().toString(36).substring(7),
+                clusterizer: null,
+            }
+        },
+        created() {
+            this.initializeClusterizer();
+        },
+        mounted() {
+            this.initializeTracksTable();
+        },
+        methods: {
+            initializeClusterizer() {
+                const clusterizer = new DesktopClusterizer();
+                clusterizer.configure({
+                    columns: columns.TRACK_TITLE,
+                });
+
+                this.clusterizer = clusterizer;
+            },
+            initializeTracksTable() {
+                const rows = this.clusterizer.generateForTracks(this.tracks);
+                this._initClusterizeJs(rows);
+            },
+            _initClusterizeJs(rows) {
+                new Clusterize({
+                    scrollId: this.scrollId,
+                    contentId: this.contentId,
+                    rows,
+                    callbacks: {
+                        // clusterChanged: () => this.clusterChanged()
+                    }
+                });
+            }
+        },
+        computed: {
+            scrollId() {
+                return this.tableId + '-scroll';
+            },
+            contentId() {
+                return this.tableId + '-content';
+            },
+        }
     }
 </script>
 
--- resources/js/tests/spec/clusterizer/BaseTrackTable.spec.js
+++ resources/js/tests/spec/clusterizer/BaseTrackTable.spec.js
@@ -1,11 +1,22 @@
-import { mount } from '@vue/test-utils';
+import {mount} from '@vue/test-utils';
 import BaseTrackTable2 from "$scripts/components/TrackTable/BaseTrackTable2";
 
 describe('BaseTrackTable', () => {
     it('should should contain divs needed for clusterize.js', () => {
-        const wrapper = mount(BaseTrackTable2);
+        const wrapper = mount(BaseTrackTable2, {
+            propsData: {
+                tracks: [],
+            },
+            methods: {
+                _initClusterizeJs: () => undefined
+            },
+            sync: true,
+        });
 
-        expect(wrapper.contains('.clusterize-scroll')).toBe(true);
-        expect(wrapper.contains('.clusterize-content')).toBe(true);
+        const scrollId = 'div#' + wrapper.vm.scrollId;
+        const contentId = 'div#' + wrapper.vm.contentId;
+
+        expect(wrapper.contains(scrollId)).toBe(true);
+        expect(wrapper.contains(contentId)).toBe(true);
     });
 });

35b29033f2e79741e507cb3b6c9443856340cd43
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2019-10-27 14:19:00 +0100

    CacheRequest: does not add track with same id twice.

--- resources/js/tests/spec/CacheRequest/CacheRequest.spec.js
+++ resources/js/tests/spec/CacheRequest/CacheRequest.spec.js
@@ -15,6 +15,18 @@ describe('CacheRequest', () => {
         done();
     });
 
+    it('should not add track twice', async done => {
+        const name = 'track name';
+        const track = {id: 1, name};
+        cacheRequest.addTracks(track);
+        cacheRequest.addTracks({id: 1, name: 'other name'});
+
+        const retrievedTrack = await cacheRequest.getTrack(1);
+
+        expect(retrievedTrack.name).toBe(name);
+        done();
+    });
+
     it('should return cached tracks as Promise', async done => {
         const track1 = {id: 1};
         const track2 = {id: 2};

4fe8240f3b03a18a87703acf5b2d5434b6437b2b
Matthias Schwebler <m.schwebler@laola1.at>
2019-11-22 16:16:15 +0100

    Undos async css loading as it is not supported by firefox.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -2,19 +2,15 @@
 <html lang="{{ app()->getLocale() }}" class="has-navbar-fixed-top">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="Description" content="Overengineering a music player!">
 
     <title>{{ config('app.name', 'MotzlMusic') }}</title>
 
     <link rel="stylesheet" href="/css/preloader.css">
-    <link rel="preload" href="/css/app.css" as="style" onload="this.rel='stylesheet'">
-    <link
-        rel="preload"
-        href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Quicksand|Source+Sans+Pro:300,400,600,700|Titillium+Web:300,400,600,700&display=swap"
-        as="style"
-        onload="this.rel='stylesheet'">
+    <link rel="stylesheet" href="/css/app.css">
+    <link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Quicksand|Source+Sans+Pro:300,400,600,700|Titillium+Web:300,400,600,700&display=swap" rel="stylesheet">
     <script src="{{ mix('/js/app.js') }}" defer></script>
     <script src="/js/greenSock.js" async></script>
     <script src="/js/spotify-player.js" async></script>

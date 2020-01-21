df521d72cc1fdaab5695a46afec9d728edcf74f2
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2020-01-21 20:00:02 +0100

    Adds local player for hosted track files.

--- resources/js/store/modules/player/helpers/v2/providers/LocalProvider.js
+++ resources/js/store/modules/player/helpers/v2/providers/LocalProvider.js
@@ -1,35 +1,75 @@
 const identifier = 'local';
-import axios from 'axios';
 import player from '../player';
 
 export default class SpotifyProvider {
 
     constructor(audioElement) {
+        this.progress = 0;
+        this.progressPercent = 0;
         this._audioElement = audioElement;
+        this._pollInterval = null;
     }
 
     async play(track) {
-        console.log('play', track);
+        this._audioElement.src = this._getAudioSource(track.id);
+        await this._audioElement.play();
+        this._subscribeToProgress();
     }
 
     async pause() {
-        console.log('pause');
+        await this._audioElement.pause();
+        this._unsubscribeFromProgress();
     }
 
     async resume() {
-        console.log('resume');
+        if (this._audioElement.src) {
+            await this._audioElement.play();
+            this._subscribeToProgress();
+        }
     }
 
     async seek(ms) {
-        console.log('seek');
+        this._audioElement.currentTime = ms / 1000;
     }
 
     async setVolume(volume) {
-        console.log('setVolume');
+        this._audioElement.volume = volume;
     }
 
     get identifier() {
         return identifier;
     }
 
+    _getAudioSource(trackId) {
+        return '/file/track/' + trackId;
+    }
+
+    _subscribeToProgress() {
+        this._pollInterval = setInterval(() => {
+            this._handleProgress(this._audioElement.currentTime * 1000);
+        }, 1000);
+    }
+
+    _unsubscribeFromProgress() {
+        clearInterval(this._pollInterval);
+    }
+
+    _handleProgress(progressMs) {
+        const total = player.currentTrack.duration;
+
+        this._setProgressPercent(total, progressMs);
+        this._playNextIfNeeded(total, progressMs);
+    }
+
+    _setProgressPercent(total, current) {
+        this.progress = current;
+        this.progressPercent = Math.round((100 * current / total) * 1000) / 1000;
+    }
+
+    _playNextIfNeeded(total, current) {
+        if (total - current <= 1000) {
+            this._unsubscribeFromProgress();
+            player.playNext();
+        }
+    }
 }

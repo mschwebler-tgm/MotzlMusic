51feb8c67e7f55816c425d811f4009f21d2a0c01
Matthias Schwebler <mschwebler@student.tgm.ac.at>
2020-01-23 19:32:09 +0100

    Revert "Increases sort performance."
    This reverts commit 9a0c036d

--- app/Http/Requests/Sort.php
+++ app/Http/Requests/Sort.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests;
 
-use DB;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Query\JoinClause;
 
 class Sort
 {
@@ -27,32 +25,6 @@ class Sort
 
     public function apply(Builder $query)
     {
-        $this->joinNecessaryField($query);
         $query->orderBy(self::FIELD_BY_IDENTIFIER[$this->identifier], $this->direction);
-    }
-
-    private function joinNecessaryField(Builder $query)
-    {
-        switch ($this->identifier) {
-            case 'rating':
-                $query
-                    ->leftJoin('user_track_ratings', function (JoinClause $query) {
-                        $query->on('tracks.id', '=', 'user_track_ratings.track_id');
-                        $query->where('user_track_ratings.user_id', '=', apiUser()->id);
-                    })
-                    ->groupBy('tracks.id')
-                    ->addSelect(DB::raw('MAX(user_track_ratings.rating) AS ' . self::FIELD_BY_IDENTIFIER['rating']));
-                break;
-            case 'artist':
-                $query
-                    ->leftJoin('track_has_artist', 'track_has_artist.track_id', '=', 'tracks.id')
-                    ->leftJoin('artists', 'artists.id', '=', 'track_has_artist.artist_id')
-                    ->groupBy('tracks.id')
-                    ->addSelect('MAX(artists.name) AS ' . self::FIELD_BY_IDENTIFIER['artist']);
-                break;
-            case 'album':
-                $query->leftJoin('albums', 'albums.id', '=', 'tracks.album_id')
-                    ->addSelect('albums.name AS ' . self::FIELD_BY_IDENTIFIER['album']);
-        }
     }
 }
